f364ae18b339d0956295c1f9fc0887366bd3b3d3
Erin Stevenson <estevenson_94@live.com>
2019-02-04 16:58:12 +0000

    ability to loop through mutliple numbers

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,24 @@
 describe("FizzBuzz", function() {
+  var fizzBuzz;
+
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  });
+
   it ("returns 'Fizz' when passed 3", function() {
-    expect(fizzBuzz(3)).toEqual('Fizz');
+    expect(fizzBuzz.play(3)).toEqual('Fizz');
    });
+
   it ("returns 'Buzz' when passed 5", function() {
-    expect(fizzBuzz(5)).toEqual('Buzz');
+    expect(fizzBuzz.play(5)).toEqual('Buzz');
    });
+
   it ("returns 'FizzBuzz' when passed 15", function() {
-    expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
    });
+
   it ("returns 7 when passed 7", function() {
-    expect(fizzBuzz(7)).toEqual(7);
+    expect(fizzBuzz.play(7)).toEqual(7);
    });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,15 +1,25 @@
-var fizzBuzz = function(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return 'FizzBuzz';
-  }
-  else if (num % 5 === 0) {
-    return 'Buzz';
-  }
-  else if (num % 3 === 0) {
-    return 'Fizz';
-  }
-  else {
-    return num;
-  }
+function FizzBuzz() {
 }
-  
+
+FizzBuzz.prototype.play = function(i) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      return 'FizzBuzz';
+    }
+    else if (i % 5 === 0) {
+      return 'Buzz';
+    }
+    else if (i % 3 === 0) {
+      return 'Fizz';
+    }
+    else {
+      return i;
+    }
+  }
+
+var fizzbuzz = new FizzBuzz();
+
+FizzBuzz.prototype.game = function(num) {
+  for (i = 0; i < num; i++) {
+    console.log(fizzBuzz.play(i));
+  }
+}  
